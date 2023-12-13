0ad29e11f133eb9c42af1b184e357fa5f0c42ef1
Bacon_Space <thespaceguild@gmail.com>
2023-12-13 13:51:39 -0500

    Delete submit.js

--- submit.js
+++
@@ -1,6 +0,0 @@
-    // Add event listener for Enter key press
-    document.getElementById("searchInput").addEventListener("keyup", function(event) {
-        if (event.key === "Enter") {
-            searchCosmetics();
-        }
-    });
